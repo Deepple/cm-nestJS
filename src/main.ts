bdc8e3ad983f9b69be1cbcf819e8c4c676a1e748
Min-Ch <cm9542@gmail.com>
2023-01-31 16:11:46 +0900

    pg bigint 문제 해결

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { types } from 'pg';
 
 declare const module: any;
 async function bootstrap() {
+  types.setTypeParser(types.builtins.INT8, (val) => parseInt(val));
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
